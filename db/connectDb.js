1f9001f3aae9280346c5b433212513fe45d0c3c0
harshchill <harshmahto02@gmail.com>
2025-08-08 11:49:50 +0530

    ready for vercel deployment

--- db/connectDb.js
+++ db/connectDb.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb://localhost:27017/Chai-addicts', {
+    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Chai-addicts', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
