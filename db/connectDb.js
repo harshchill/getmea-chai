462ae092d40cfe3619f5feba6aa7cc326ded4054
harshchill <harshmahto02@gmail.com>
2025-08-09 15:46:02 +0530

    DB fix

--- db/connectDb.js
+++ db/connectDb.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect( process.env.MONGODB_URI);
+    const conn = await mongoose.connect( process.env.MONGODB_URI, { dbName: 'Chai-addicts' });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error('MongoDB connection error:', error.message);
