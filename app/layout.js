91bad0ca905b961996978eb8bbef026d1f15c7e0
harshchill <harshmahto02@gmail.com>
2025-08-09 08:48:56 +0530

    web analytics

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Nav from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import SessionWrapper from "@/components/SessionWrapper";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
